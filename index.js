ffeb41f4443bc0d6dc006b1e44635a2b88e4c6fd
Mustafa <124535608+MUChappy@users.noreply.github.com>
2024-07-23 19:13:28 -0400

    Refactored original code to use express

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-const http = require('http');
-const url = require('url');
+const express = require('express');
+const path = require('path');
+// const url = require('url');
 const fs = require('fs');
 
+const app = express();
 const PAGES = ['/', '/index', '/about', '/contact-me'];
 
-http.createServer((req, res) => {
-  const q = url.parse(req.url, true);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log('Did it work?'));
 
-  let filename;
-  if (!PAGES.includes(q.pathname)) filename = './404.html';
-  else 
-    q.pathname === '/' ? filename = './index.html' : filename = '.' + q.pathname + '.html';
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, '/about.html')));
+app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, '/contact-me.html')));
 
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end(`${err}`);
-    }
+app.use((req, res, next) => res.sendFile(path.join(__dirname, '/404.html')));
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  })
-}).listen(8080);
+
+// app.get('/',(req, res) => {
+//   const data = readHTML('./index.html');
+//   if (data) {
+//     res.send(data);
+//   }
+// });
+
+// app.get('/about',(req, res) => {
+//   const data = readHTML('./about.html');
+//   if (data) {
+//     res.send(data);
+//   }
+// });
+
+// app.get('/contact-me',(req, res) => {
+//   const data = readHTML('./contact-me.html');
+//   if (data) {
+//     res.send(data);
+//   }
+// });
+
+
+// function readHTML(filename) {
+//   fs.readFile(filename, (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     return data;
+//   });
+// }
+
+// http.createServer((req, res) => {
+//   const q = url.parse(req.url, true);
+
+//   let filename;
+//   if (!PAGES.includes(q.pathname)) filename = './404.html';
+//   else 
+//     q.pathname === '/' ? filename = './index.html' : filename = '.' + q.pathname + '.html';
+
+//   fs.readFile(filename, (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       res.writeHead(404, {'Content-Type': 'text/html'});
+//       return res.end(`${err}`);
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     res.end();
+//   })
+// }).listen(8080);
